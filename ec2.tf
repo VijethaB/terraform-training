d96adc64f600b2d9aea20e4ada69e044d6bdae04
VijethaB <68103361+VijethaB@users.noreply.github.com>
2022-07-08 13:08:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,28 @@
-resource "aws_instance" "myawsserver" {
-  ami = var.image
-  instance_type = "${ var.env == "prod" ? "t2.medium" : "t2.micro" }"
-
-  tags = {
-    Name = var.name
-    Env = var.env
-    Owner = "vij"
-  }
-}
-
-output "myawsserver-ip" {
+output "public-ip" {
   value = aws_instance.myawsserver.public_ip
 }
 
-output "myserver-PrivateIP" {
+output "private-ip" {
   value = aws_instance.myawsserver.private_ip
+}
+
+variable "image" {
+  type = string
+  default = "ami-02f3416038bdb17fb"
+}
+
+variable "hw" {
+  default = "t2.micro"
+}
+
+variable "name" {
+  default = "gagan-manual-tf"
+}
+
+variable "env" {
+  default = "prod"
+}
+
+variable "bucket-name" {
+  default = "gagandefaultbucket"
 }
