625517ba177e780ce36a6166b40d14bc9fb826b7
VijethaB <68103361+VijethaB@users.noreply.github.com>
2022-07-08 13:09:24 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,28 @@
+output "public-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
+
+output "private-ip" {
+  value = aws_instance.myawsserver.private_ip
+}
+
 variable "image" {
   type = string
-  default = "ami-0603cbe34fd08cb81"
+  default = "ami-02f3416038bdb17fb"
 }
 
 variable "hw" {
   default = "t2.micro"
 }
 
+variable "name" {
+  default = "gagan-manual-tf"
+}
+
 variable "env" {
   default = "prod"
 }
 
-variable "name" {
-  default = "vij-server"
+variable "bucket-name" {
+  default = "gagandefaultbucket"
 }
-
